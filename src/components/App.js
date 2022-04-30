4dbd9937bbf0c92465fd26fd0da11971e68b989b
Kyrian <dr.kyrian.onwe@gmail.com>
2022-04-29 20:38:09 -0500

    commit tree f476510935258c09bdc288d33bd265ccabf9795a

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import { useState, useEffect } from "react"
+
+function App(){
+    const url = "https://dog.ceo/api/breeds/image/random"
+    const [screenState, setScreenState] = useState({})
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            setScreenState(data);
+            setIsLoaded(true)
+            //console.log(screenState.message)
+          });
+      }, []);
+
+      if (!isLoaded) return <p>Loading...</p>;
+
+      
+      return <div>
+          <img src={screenState.message} alt={"A Random Dog"} />
+    
+
+      </div>
+
+
+}
+
+
+export default App;
